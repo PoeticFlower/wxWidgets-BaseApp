bed997742c4d156bc470e84c760d9da873c9fde6
Peixuan Zhang <zhang@poeticflower.cn>
2025-02-08 10:04:31 +0800

    wxAppDemo: Add an initial version of MainApp.cpp.

---
+++ source/wxAppDemo/MainApp.cpp
@@ -0,0 +1,28 @@
+﻿/*****************************************************************************
+ *
+ * Copyright (C) 2015-2025 Poetic Flower Studio. All Rights Reserved.
+ *
+ * CONFIDENTIALITY:  This software source code is considered confidential
+ * information. It must be kept confidential in accordance with the terms
+ * and conditions of your Software License Agreement.
+ *
+ *****************************************************************************/
+
+#include <wx/wx.h>
+#include "GlobalConstants.h"
+#include "MainFrame.h"
+#include "MainApp.h"
+
+bool MainApp::OnInit()
+{
+  MainFrame* frame = new MainFrame(gcStringApplicationTitle);
+  frame->Show(true);
+  return true;
+}
+
+int MainApp::OnExit()
+{
+  return wxApp::OnExit();
+}
+
+wxIMPLEMENT_APP(MainApp);
