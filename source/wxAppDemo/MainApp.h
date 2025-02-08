e0057d5ba3e668be5bed931faa8989bbb1e7451f
Peixuan Zhang <zhang@poeticflower.cn>
2025-02-08 09:19:29 +0800

    wxAppDemo: Add an initial version of MainApp.h.

---
+++ source/wxAppDemo/MainApp.h
@@ -0,0 +1,21 @@
+﻿/*****************************************************************************
+ *
+ * Copyright (C) 2015-2025 Poetic Flower Studio. All Rights Reserved.
+ *
+ * CONFIDENTIALITY:  This software source code is considered confidential
+ * information. It must be kept confidential in accordance with the terms
+ * and conditions of your Software License Agreement.
+ *
+ *****************************************************************************/
+
+#ifndef   GUI_WXWIDGETS_MAIN_APP_H_
+#define   GUI_WXWIDGETS_MAIN_APP_H_
+
+class MainApp : public wxApp
+{
+public:
+  virtual bool OnInit();
+  virtual int  OnExit();
+};
+
+#endif // GUI_WXWIDGETS_MAIN_APP_H_
