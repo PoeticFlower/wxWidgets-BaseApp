5820e3fe20f79065c339398c0c8ad3e035b073cc
Peixuan Zhang <zhang@poeticflower.cn>
2025-02-08 09:14:32 +0800

    wxAppDemo: Add an initial version of GlobalConstants.cpp.

---
+++ source/wxAppDemo/GlobalConstants.cpp
@@ -0,0 +1,14 @@
+﻿/*****************************************************************************
+ *
+ * Copyright (C) 2015-2025 Poetic Flower Studio. All Rights Reserved.
+ *
+ * CONFIDENTIALITY:  This software source code is considered confidential
+ * information. It must be kept confidential in accordance with the terms
+ * and conditions of your Software License Agreement.
+ *
+ *****************************************************************************/
+
+#include <wx/wx.h>
+#include "GlobalConstants.h"
+
+const wxString gcStringApplicationTitle = "wxApplication Demo";
