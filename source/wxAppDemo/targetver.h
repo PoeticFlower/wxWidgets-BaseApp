7310f56f5e3bf51fbd5feedc89072fc06d92abfb
Peixuan Zhang <zhang@poeticflower.cn>
2025-02-08 11:47:32 +0800

    wxAppDemo: Add an initial version of targetver.h.

---
+++ source/wxAppDemo/targetver.h
@@ -0,0 +1,23 @@
+﻿/*****************************************************************************
+ *
+ * Copyright (C) 2015-2025 Poetic Flower Studio. All Rights Reserved.
+ *
+ * CONFIDENTIALITY:  This software source code is considered confidential
+ * information. It must be kept confidential in accordance with the terms
+ * and conditions of your Software License Agreement.
+ *
+ *
+ * FILE    : targetver.h
+ * PURPOSE : Including SDKDDKVer.h defines the highest available Windows
+ *           platform.
+ * AUTHOR  : Peixuan Zhang <Zhang@PoeticFlower.CN>
+ * COMMENTS: References the example code in Visual Studio.
+ *           If you wish to build  your  application  for a previous Windows
+ *           platform, include WinSDKVer.h and set the _WIN32_WINNT macro to
+ *           the platform you wish to support  before including SDKDDKVer.h.
+ *
+ *****************************************************************************/
+
+#pragma once
+
+#include <SDKDDKVer.h>
