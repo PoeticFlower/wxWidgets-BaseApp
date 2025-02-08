c36ac7db864213653644f69a446d5ac3a48ae35f
Peixuan Zhang <zhang@poeticflower.cn>
2025-02-08 09:20:03 +0800

    wxAppDemo: Add an initial version of MainFrame.h.

---
+++ source/wxAppDemo/MainFrame.h
@@ -0,0 +1,25 @@
+﻿/*****************************************************************************
+ *
+ * Copyright (C) 2015-2025 Poetic Flower Studio. All Rights Reserved.
+ *
+ * CONFIDENTIALITY:  This software source code is considered confidential
+ * information. It must be kept confidential in accordance with the terms
+ * and conditions of your Software License Agreement.
+ *
+ *****************************************************************************/
+
+#ifndef   GUI_WXWIDGETS_MAIN_FRAME_H_
+#define   GUI_WXWIDGETS_MAIN_FRAME_H_
+
+class MainFrame : public wxFrame
+{
+public:
+  MainFrame(const wxString& title);
+private:
+  void InitializeMenuBar();
+  void InitializeMessageBinding();
+public:
+  void OnMenuFileExit                              (wxCommandEvent& event);
+};
+
+#endif // GUI_WXWIDGETS_MAIN_FRAME_H_
