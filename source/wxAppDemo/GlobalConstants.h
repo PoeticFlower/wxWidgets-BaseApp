2d88a1e2321b0f155bc5c6b41f20f48821c3f5ac
Peixuan Zhang <zhang@poeticflower.cn>
2025-02-08 09:14:17 +0800

    wxAppDemo: Add an initial version of GlobalConstants.h.

---
+++ source/wxAppDemo/GlobalConstants.h
@@ -0,0 +1,19 @@
+﻿/*****************************************************************************
+ *
+ * Copyright (C) 2015-2025 Poetic Flower Studio. All Rights Reserved.
+ *
+ * CONFIDENTIALITY:  This software source code is considered confidential
+ * information. It must be kept confidential in accordance with the terms
+ * and conditions of your Software License Agreement.
+ *
+ *****************************************************************************/
+
+#ifndef   GUI_WXWIDGETS_MAIN_APP_GLOBAL_CONSTANTS_H_
+#define   GUI_WXWIDGETS_MAIN_APP_GLOBAL_CONSTANTS_H_
+
+#define ID_MENU_FILE                                                      12800
+#define ID_MENU_FILE_EXIT                                                 12801
+
+extern const wxString gcStringApplicationTitle;
+
+#endif // GUI_WXWIDGETS_MAIN_APP_GLOBAL_CONSTANTS_H_
